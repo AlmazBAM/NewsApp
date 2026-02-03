58aedfdc2c832d4bb70430416518e5695d39b764
Almaz <almazbagmanov@yandex.ru>
2026-02-03 21:11:22 +0300

    Creating notifications about article updates

--- sync/src/main/java/com/bagmanov/sync/NewsWorkManager.kt
+++ sync/src/main/java/com/bagmanov/sync/NewsWorkManager.kt
@@ -7,6 +7,7 @@ import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkerParameters
 import com.bagmanovam.domain.model.RefreshConfig
 import com.bagmanovam.domain.usecase.UpdateArticlesForAllSubscriptionsUseCase
+import com.bagmanovam.notifications.Notifier
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.util.concurrent.TimeUnit
@@ -15,13 +16,15 @@ private const val TAG = "NewsWorkManager"
 class NewsWorkManager(
     context: Context,
     params: WorkerParameters,
-    private val updateArticlesForAllSubscriptionsUseCase: UpdateArticlesForAllSubscriptionsUseCase
+    private val updateArticlesForAllSubscriptionsUseCase: UpdateArticlesForAllSubscriptionsUseCase,
+    private val notifier: Notifier
 ) : CoroutineWorker(context, params) {
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         Log.i(TAG, "doWork: start", )
         try {
             val topics = updateArticlesForAllSubscriptionsUseCase()
             Log.i(TAG, "doWork: finish success")
+            notifier.postNotifications(topics)
             Result.success()
         } catch (e: Exception) {
             Log.i(TAG, "doWork: finish error ${e.message}")
