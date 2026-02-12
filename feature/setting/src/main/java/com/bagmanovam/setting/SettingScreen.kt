f2fb8333e93d46f7394c6f7a21c5cda7a649a16a
Almaz <almazbagmanov@yandex.ru>
2026-02-12 21:21:24 +0300

    Added thumbcontent for switches, refactor settingRow class

--- feature/setting/src/main/java/com/bagmanovam/setting/SettingScreen.kt
+++ feature/setting/src/main/java/com/bagmanovam/setting/SettingScreen.kt
@@ -2,7 +2,10 @@ package com.bagmanovam.setting
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
@@ -12,6 +15,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.bagmanovam.setting.component.AccountCard
 import com.bagmanovam.setting.component.PreferenceCard
 import com.bagmanovam.setting.component.SettingsSection
 import com.bagmanovam.ui.NewsAppTheme
@@ -32,6 +36,25 @@ fun SettingScreen(
         LazyColumn(
             modifier = Modifier.matchParentSize()
         ) {
+
+            item {
+                SettingsSection(
+                    modifier = Modifier.padding(bottom = 12.dp),
+                    title = stringResource(R.string.account_section)
+                )
+            }
+
+            item {
+                AccountCard(
+                    modifier = Modifier.background(MaterialTheme.colorScheme.surface),
+                    onSettingAction = onSettingAction
+                )
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
             item {
                 SettingsSection(
                     modifier = Modifier.padding(bottom = 12.dp),
--- feature/setting/src/main/java/com/bagmanovam/setting/component/SettingRow.kt
+++ feature/setting/src/main/java/com/bagmanovam/setting/component/SettingRow.kt
@@ -1,25 +1,18 @@
 package com.bagmanovam.setting.component
 
-import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Newspaper
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Wifi
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.bagmanovam.setting.R
 
 
 @Composable
@@ -51,20 +44,3 @@ fun SettingRow(
     }
 }
 
-
-enum class ToggleItem(val icon: ImageVector) {
-    NOTIFICATION_ENABLED(Icons.Default.Notifications),
-    WIFI_ONLY(Icons.Default.Wifi),
-
-    THEME(Icons.Filled.Newspaper),
-
-}
-
-fun ToggleItem.toDisplayableString(context: Context): String {
-    return when (this) {
-        ToggleItem.NOTIFICATION_ENABLED -> context.getString(R.string.notification_enabled)
-        ToggleItem.WIFI_ONLY -> context.getString(R.string.wifi_only)
-        ToggleItem.THEME -> context.getString(R.string.theme)
-
-    }
-}
